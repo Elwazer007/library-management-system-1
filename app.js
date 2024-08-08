c87d02b1558c2b4ff1b38e4537a7af6f75df785e
Mohamed Ibrahim <mohamedwazer@paymob.com>
2024-08-08 23:13:05 +0300

    Add swagger docs

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const swaggerJsDoc = require('./docs/swagger');
+
 app.use(express.json());
 
 const bookRoutes = require('./routes/bookRoutes');
@@ -8,6 +10,8 @@ const borrowerRoutes = require('./routes/borrowerRoutes');
 const borrowingRoutes = require('./routes/borrowingRoutes');
 const reportRoutes = require('./routes/reportsRoutes');
 const userRoutes = require('./routes/userRoutes');
+
+app.use('/api-docs', swaggerJsDoc.serve, swaggerJsDoc.setup);
 
 
 app.use('/api/books', bookRoutes);
---
+++ docs/swagger.js
@@ -0,0 +1,39 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Library Management System API',
+      version: '1.0.0',
+      description: 'A simple Express Library API',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        }
+      }
+    },
+    security: [{
+      bearerAuth: []
+    }]
+  },
+  apis: ['./docs/**/*.yaml'], // Path to the API docs
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = {
+  serve: swaggerUi.serve,
+  setup: swaggerUi.setup(specs)
+};
